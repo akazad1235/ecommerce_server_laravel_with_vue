efa6f16bf80c007f1e8eaf4b2c7c7bef0269d51d
akazad1235 <akazad914@gmail.com>
2021-07-14 02:54:00 +0600

    categories api created

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -14,7 +15,8 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+      $getAllCats =   Category::get();
+      return response()->json(['data'=>$getAllCats, 'status'=>200]);
     }
 
     /**
